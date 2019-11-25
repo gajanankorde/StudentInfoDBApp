93e58d121926da23c1f3e84c810f249a3b75577d
gajanan <gajanankorde.cse@gmail.com>
2019-11-25 23:11:07 +0530

    Initial commit

---
+++ app/src/main/java/com/gajanan/example/studentinfodbapp/DatabaseHelper.java
@@ -0,0 +1,179 @@
+package com.gajanan.example.studentinfodbapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public final static String DATBASE_NAME = "Student.db";
+
+    public final static String TABLE_NAME = "student_table";
+
+    public final static String COL_1 = "ID";
+    public final static String COL_2 = "NAME";
+    public final static String COL_3 = "EMAIL";
+    public final static String COL_4 = "MOBILE_NUMBER";
+
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATBASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS "
+                + TABLE_NAME+
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                " NAME TEXT," +
+                " EMAIL TEXT," +
+                " MOBILE_NUMBER INTEGER)");
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(String name, String email, String mobNumber){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, email);
+        contentValues.put(COL_4, mobNumber);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1){
+            return false;
+        } else {
+            return true;
+        }
+
+
+    }
+
+    public Integer updateData(String id, String name, String email, String mobNumber){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, email);
+        contentValues.put(COL_4, mobNumber);
+
+        return db.update(TABLE_NAME, contentValues, "ID=?", new String[]{id});
+
+
+    }
+
+
+    public Cursor getData(String id){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM "+TABLE_NAME+" WHERE ID='"+id+"'";
+        Cursor cursor = db.rawQuery(query, null);
+
+        return cursor;
+
+
+    }
+
+    public Integer deleteData(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        return db.delete(TABLE_NAME, "ID=?", new String[]{id});
+
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM "+TABLE_NAME, null);
+        return cursor;
+    }
+
+
+    public boolean checkUser(String email) {
+
+        // array of columns to fetch
+        String[] columns = {
+                COL_1
+        };
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // selection criteria
+        String selection = COL_3 + " = ?";
+
+        // selection argument
+        String[] selectionArgs = {email};
+
+        // query user table with condition
+        /**
+         * Here query function is used to fetch records from user table this function works like we use sql query.
+         * SQL query equivalent to this query function is
+         * SELECT user_id FROM user WHERE user_email = 'jack@androidtutorialshub.com';
+         */
+        Cursor cursor = db.query(TABLE_NAME, //Table to query
+                columns,                    //columns to return
+                selection,                  //columns for the WHERE clause
+                selectionArgs,              //The values for the WHERE clause
+                null,                       //group the rows
+                null,                      //filter by row groups
+                null);                      //The sort order
+        int cursorCount = cursor.getCount();
+        cursor.close();
+        db.close();
+
+        if (cursorCount > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean checkUserId(String id) {
+
+        // array of columns to fetch
+        String[] columns = {
+                COL_1
+        };
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // selection criteria
+        String selection = COL_1 + " = ?";
+
+        // selection argument
+        String[] selectionArgs = {id};
+
+        // query user table with condition
+        /**
+         * Here query function is used to fetch records from user table this function works like we use sql query.
+         * SQL query equivalent to this query function is
+         * SELECT user_id FROM user WHERE user_email = 'jack@androidtutorialshub.com';
+         */
+        Cursor cursor = db.query(TABLE_NAME, //Table to query
+                columns,                    //columns to return
+                selection,                  //columns for the WHERE clause
+                selectionArgs,              //The values for the WHERE clause
+                null,                       //group the rows
+                null,                      //filter by row groups
+                null);                      //The sort order
+        int cursorCount = cursor.getCount();
+        cursor.close();
+        db.close();
+
+        if (cursorCount > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
---
+++ app/src/main/java/com/gajanan/example/studentinfodbapp/MainActivity.java
@@ -0,0 +1,253 @@
+package com.gajanan.example.studentinfodbapp;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDB;
+    EditText edtId, edtName, edtEmail, edtMobNumber;
+    Button btnAdd, btnGetData, btnUpdate, btnDelete, btnViewAll, btnClear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myDB = new DatabaseHelper(this);
+
+        edtId = findViewById(R.id.edt_id);
+        edtName = findViewById(R.id.edt_name);
+        edtEmail = findViewById(R.id.edt_email);
+        edtMobNumber = findViewById(R.id.edt_mobNumber);
+
+        btnAdd = findViewById(R.id.btn_add);
+        btnDelete = findViewById(R.id.btn_delete);
+        btnUpdate = findViewById(R.id.btn_update);
+        btnGetData = findViewById(R.id.btn_view);
+        btnViewAll = findViewById(R.id.btn_viewAll);
+
+        btnClear = findViewById(R.id.btn_clear);
+
+        edtName.requestFocus();
+
+
+        addData();
+        getData();
+        viewAll();
+        updateData();
+        deleteData();
+
+        clearData();
+
+
+    }
+
+    public void addData(){
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name= edtName.getText().toString();
+                String email= edtEmail.getText().toString();
+                String mobNumber= edtMobNumber.getText().toString();
+
+                if (name.isEmpty() || email.isEmpty() || mobNumber.isEmpty()){
+
+                    Toast.makeText(MainActivity.this, "Please fill out all details", Toast.LENGTH_LONG).show();
+                }
+                else {
+
+                    if (!myDB.checkUser(email)){
+
+                        boolean isInserted = myDB.insertData(name,email,mobNumber);
+                        if (isInserted == true){
+                            Toast.makeText(MainActivity.this, "Data Inserted!", Toast.LENGTH_SHORT).show();
+                            clear();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Email Already Exists", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+
+            }
+        });
+    }
+    public void getData(){
+
+        btnGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String id = edtId.getText().toString();
+
+                if (id.equals(String.valueOf(""))){
+                    edtId.setError("Enter ID");
+
+                    return;
+                }
+
+
+                if (myDB.checkUserId(id)){
+
+                    Cursor cursor = myDB.getData(id);
+
+                    String name="";
+                    String email="";
+                    String mobNumber="";
+
+                    if (cursor.moveToNext()){
+
+                        id=cursor.getString(0);
+                        name=cursor.getString(1);
+                        email=cursor.getString(2);
+                        mobNumber=cursor.getString(3);
+                    }
+
+                    edtId.setText(id);
+                    edtName.setText(name);
+                    edtEmail.setText(email);
+                    edtMobNumber.setText(mobNumber);
+
+
+                }else {
+                    Toast.makeText(MainActivity.this, "User Does not Exists", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+    }
+    public void viewAll(){
+
+        btnViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Cursor cursor = myDB.getAllData();
+
+                //small test
+                if (cursor.getCount() == 0){
+                    showMessage("Error", "Nothing found in DB");
+                    return;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+
+                while (cursor.moveToNext()){
+                    buffer.append("ID: "+cursor.getString(0)+"\n");
+                    buffer.append("Name: "+cursor.getString(1)+"\n");
+                    buffer.append("Email: "+cursor.getString(2)+"\n");
+                    //buffer.append("CC: "+cursor.getString(3)+"\n\n");
+                    buffer.append("Mobile Number: "+cursor.getString(3)+"\n\n");
+                }
+                showMessage("All data", buffer.toString());
+
+            }
+        });
+
+
+    }
+    public void updateData(){
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String id= edtId.getText().toString();
+                String name= edtName.getText().toString();
+                String email= edtEmail.getText().toString();
+                String mobNumber= edtMobNumber.getText().toString();
+
+                if (id.isEmpty() || name.isEmpty() || email.isEmpty() || mobNumber.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please fill out all details", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Integer updateRow = myDB.updateData(id, name, email, mobNumber);
+
+                    if (updateRow > 0){
+                        Toast.makeText(MainActivity.this, "Updated successfully", Toast.LENGTH_SHORT).show();
+                        clear();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Invalid id", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+
+
+            }
+        });
+    }
+    public void deleteData(){
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String id= edtId.getText().toString();
+
+                if (id.isEmpty()){
+                    edtId.setError("Please Enter ID");
+                    return;
+                }
+                else{
+
+                    Integer deletedRow = myDB.deleteData(edtId.getText().toString());
+
+                    if (deletedRow > 0){
+                        Toast.makeText(MainActivity.this, "Delete Success", Toast.LENGTH_SHORT).show();
+                    } else {
+                        //Toast.makeText(MainActivity.this, "OOPSSS!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Invalid ID", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+    }
+
+
+    private void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.create();
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+    public void clear(){
+        edtId.setText("");
+        edtName.setText("");
+        edtEmail.setText("");
+        edtMobNumber.setText("");
+    }
+
+    public void clearData(){
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtId.setText("");
+                edtName.setText("");
+                edtEmail.setText("");
+                edtMobNumber.setText("");
+
+            }
+        });
+    }
+
+}
